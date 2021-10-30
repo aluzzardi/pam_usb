5515d403720ce2436befa0577dad76beacb5b744
McDope <TobiasBaeumer@gmail.com>
2021-10-30 11:55:27 +0200

    #75: Improve PolicyKit compatibility (#77)
    * #72: Fix screen part in DISPLAY env var causing utmp check to fail (#73)
    
    Some enviroments, for example XFCE, set DISPLAY to a value including the screen identifier. This is causing the utmp check to fail since the session is registered using only the display part.
    
    This fixes this by cutting the screen identifier from the read env var.
    
    * #75: local check: get DISPLAY from polkit-agent-helper if found
    
    * Revert "#75: local check: get DISPLAY from polkit-agent-helper if found"
    
    This reverts commit d2351510ff25ec64d56bb97981eca1ed57059a71.
    
    * #75: Whitelist polkit since environ is not accessible under pkexec

--- src/local.c
+++ src/local.c
@@ -199,8 +199,17 @@ int pusb_local_login(t_pusb_options *opts, const char *user, const char *service
 		}
 	}
 
-	if (strcmp(service, "gdm-password") == 0 || strcmp(service, "xdm") == 0 || strcmp(service, "lightdm") == 0 || strcmp(service, "sddm") == 0) {
-		log_debug("	Graphical login request detected, assuming local.");
+	/**
+	 * These services are whitelisted because either a) they are graphical login managers and we assume these
+	 * to be available only locally or b) they are authorization agents afters successful authentication.
+	 */
+	if (strcmp(service, "gdm-password") == 0 ||
+		strcmp(service, "xdm") == 0 ||
+		strcmp(service, "lightdm") == 0 ||
+		strcmp(service, "sddm") == 0 ||
+		strcmp(service, "polkit-1") == 0
+	) {
+		log_debug("	Whitelisted request by %s detected, assuming local.\n", service);
 		local_request = 1;
 	}
 
@@ -225,6 +234,13 @@ int pusb_local_login(t_pusb_options *opts, const char *user, const char *service
 
 	if (local_request == 0 && display != NULL) {
 		log_debug("	Using DISPLAY %s for utmp search\n", display);
+
+		if (strstr(display, ".0") != NULL) {
+			// DISPLAY contains not only display but also default screen, truncate screen part in this case
+			log_debug("	DISPLAY contains screen, truncating...\n");
+			memset(display + strlen(display) - 2, 0, 2);
+		}
+
 		local_request = pusb_is_tty_local((char *) display);
 
 		char *xorg_tty = (char *)malloc(32);
